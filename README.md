aaddd24ef1571a0eee2925e1bcb0e2fdf0930be9
Haneul Seong <hana@disroot.org>
2021-02-02 08:33:45 +0700

    Release

---
+++ README.md
@@ -0,0 +1,19 @@
+# pc-sambungkata
+
+Powercord plugin untuk membantu dalam bermain sambungkata minigame Alita
+
+# Install
+
+Clone ke folder plugins powercord mu
+
+``git clone https://github.com/hanahaneull/pc-sambungkata``
+
+# Penggunaan
+
+[prefix]sk [awal kata] [minimal huruf (optional)]
+
+``.sk bi 10``
+
+# Showcase
+
+![showcase](https://hana.raped.men/mFAdxz9oLYF.png)
---
+++ index.js
@@ -0,0 +1,45 @@
+const { Plugin } = require('powercord/entities')
+const { get } = require('powercord/http')
+
+module.exports = class SambungKata extends Plugin {
+  async startPlugin () {
+    powercord.api.commands.registerCommand({
+      command: 'sk',
+      description: 'Ngecheat pas main sambung-kata :vrottt',
+      usage: '{c} kata [minimal kata]',
+      executor: async args => {
+        const kata = args[0]
+        if (!kata)
+          return {
+            send: false,
+            result: 'Masukkan kata untuk dicari!'
+          }
+        const url = `https://api.hana.uno/sk?q=${kata}&l=${args[1] || 0}`
+        const res = await get(url)
+        if (res.statusCode === 200) {
+          let result = []
+          res.body.forEach(x => result.push(`**${x.word}**`))
+          return {
+            send: false,
+            result: {
+              type: 'rich',
+              title: 'Hasil',
+              description: result.join(' ^ '),
+              color: Math.floor(Math.random() * 16777215),
+              footer: {
+                text: 'https://api.hana.uno/'
+              }
+            }
+          }
+        }
+        return {
+          send: false,
+          result: 'Error'
+        }
+      }
+    })
+  }
+  pluginWillUnload() {
+    powercord.api.commands.unregisterCommand('sk')
+  }
+}
